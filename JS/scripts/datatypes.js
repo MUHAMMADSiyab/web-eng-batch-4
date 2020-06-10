b6769caea34a4c82e7d24ca0edd1228bcb08a6dc
= <=>
2020-06-10 04:29:35 -0700

    created js folder & datatypes files

---
+++ JS/scripts/datatypes.js
@@ -0,0 +1,36 @@
+/**
+ * ************** JavaScript Data Types **************
+ */
+
+// String
+var username = "naeem@123";
+
+// Number
+var age = 24.5;
+
+// Undefined
+var name = undefined;
+
+// Object (Non-Primitive)
+var users = ["john", "marry", "smith", "david"];
+
+users[0] = "naeem";
+
+var userInfo = {
+  username: "naeem123",
+  age: 24,
+  married: false,
+};
+
+// console.log(users[2]);
+// console.log(userInfo.username);
+
+// Boolean
+var married = false;
+
+married = true;
+
+// NULL
+var cricketInterest = null;
+
+console.log(users);
