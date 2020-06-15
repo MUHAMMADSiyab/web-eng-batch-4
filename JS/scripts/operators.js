763ddfb6b5cf4e1d57d84a7940b94a18b5ea795b
= <=>
2020-06-15 04:37:10 -0700

    added operators to JS directory

---
+++ JS/scripts/operators.js
@@ -0,0 +1,28 @@
+/**** JavaScript Operators  ****/
+
+var x = 3;
+var y = 3;
+
+// --------- Assignment Operators -------- //
+
+// x += y;
+// x -= y;
+// x *= 2;
+// x /= 2;
+// x %= y;
+// x **= y;
+
+// --------- Arithmetic Operators -------- //
+console.log("Addition", x + y);
+console.log("Subtraction", x - y);
+console.log("Multiplication", x * y);
+console.log("Power", x ** y);
+console.log("Division", x / y);
+console.log("Remainder", x % y);
+
+x++; // 1 is added to `x` variable
+
+console.log("X variable is incremented to", x);
+
+y--; // 1 is subtracted from `y` variable
+console.log("Y is decremented to", y);
