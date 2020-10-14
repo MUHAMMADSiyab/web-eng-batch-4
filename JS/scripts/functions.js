fb8f886c45f08e550e679541ae93fdcde5980eb8
MUHAMMAD Siyab <siyab.dev@gmail.com>
2020-10-14 04:41:44 -0700

    added two files

---
+++ JS/scripts/functions.js
@@ -0,0 +1,8 @@
+function addTwoNumbers (a, b) {
+    return a + b;
+}
+
+// var product = addTwoNumbers();
+
+console.log( addTwoNumbers(12, 10) );
+console.log( addTwoNumbers(50, 40) );
--- JS/scripts/loops.js
+++ JS/scripts/loops.js
@@ -24,5 +24,35 @@ var number = 2;
 for (var i = 1; i < 11; i++) {
   var product = number * i;
 
-  console.log(number + " x " + i + " =", product + "(" + product ** 2 + ")");
+  // console.log(number + " x " + i + " =", product + "(" + product ** 2 + ")");
+}
+
+// 2. While loop
+var i = 0;
+
+while (i < 20) {
+  i++;
+  // console.log(i);
+}
+
+// 3. do-while loop
+var index = 0;
+do {
+  index++;
+  // console.log(index);
+} while (index < -1);
+
+/**
+ * Practical Example
+ */
+var input = prompt("Enter a number");
+
+var n = 0;
+
+while (n < 10) {
+  n++;
+  var product = input * n;
+  var output = input + " x " + n + " = " + product;
+
+  console.log(output);
 }
