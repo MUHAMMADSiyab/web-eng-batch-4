0251e4385b3e4f24919933bfa18d0b5a5d255cc9
MUHAMMAD Siyab <siyab.dev@gmail.com>
2020-10-16 04:39:56 -0700

    Added string functions file

---
+++ JS/scripts/string-func.js
@@ -0,0 +1,22 @@
+var message = "   Hello World in JavaScript JavaScript    ";
+
+console.log( message.toUpperCase() );
+
+console.log( message.toLowerCase() );
+
+console.log( message.indexOf("W") );
+
+console.log( message.substr(6, 12) );
+
+console.log( message.repeat(5) );
+
+console.log( message.replace(/javascript/ig, "PHP") );
+
+console.log( message.startsWith("ABC") );
+console.log( message.endsWith("t") );
+
+console.log( message.split(" ") );
+
+console.log( message.concat(" ABC") );
+
+console.log( message.trim().length );
